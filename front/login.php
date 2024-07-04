182acf4e120698708c592c18aca0c4276bcfc416
yt Chen <yutongc30@gmail.com>
2024-07-04 10:45:07 +0800

    建立users資料表，並建立會員登入頁面

---
+++ front/login.php
@@ -0,0 +1,33 @@
+<fieldset style="width:60%;margin:20px auto">
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+                <button onclick="login()">清除</button>
+                <button onclick="clear()">登入</button>
+            </td>
+            <td>
+                <a href="?do=forget">忘記密碼</a>
+                <a href="?do=reg">尚未註冊</a>
+            </td>
+        </tr>
+    </table>
+</fieldset>
+
+<script>
+    function login() {
+
+    }
+
+    function clear() {
+
+    }
+</script>
